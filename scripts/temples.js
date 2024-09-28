cdfc1332488736b137438c8aaa96c59713a893b5
Hopeudoka <wisparex02@gmail.com>
2024-09-28 23:52:10 +0100

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,12 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+const lastUpdate = document.querySelector("#lastModified")
+lastUpdate.innerHTML = `Last Modification: ${document.lastModified}`;
+
+
 // store the selected elements in variables.
 const mainnav = document.querySelector(".navigation");
 const hambutton = document.querySelector("#menu");
